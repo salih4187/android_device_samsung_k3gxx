d0108df237f4efc36f890b8396258532257887a0
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    k3gxx: Add libbt-vendor in klte.mk
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b

--- device.mk
+++ device.mk
@@ -39,7 +39,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
 
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true

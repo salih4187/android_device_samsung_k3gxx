5e15c4a14ffd7af56fca94508c1561ad6151be05
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-11 19:58:50 +0300

    k3gxx: Add bluetooth service
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba38

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 1920

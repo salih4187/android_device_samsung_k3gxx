88e7a0171d2da7c21f5d88abe08f9c242f91f116
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-11 19:33:30 +0300

    k3gxx: Add sensors HAL impl
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba30

--- device.mk
+++ device.mk
@@ -187,6 +187,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SamsungDoze
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service

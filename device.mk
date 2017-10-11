7dbb8178a3c9e8e2ca69dbe94ddd0b8b928742bb
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-11 19:56:07 +0300

    k3gxx: Add wifi and supplicant services
    android.hardware.wifi is built by default
    
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba37

--- device.mk
+++ device.mk
@@ -235,6 +235,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf 
 
 PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
     libnetcmdiface \
     macloader \
     wifiloader \

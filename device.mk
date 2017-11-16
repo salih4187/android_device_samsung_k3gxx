21e2c4abce8d1e7c55e166cc3933f7fb87db01ca
Bruno Martins <bgcngm@gmail.com>
2017-11-16 22:40:39 +0000

    k3gxx: Include DRM service
    Change-Id: I4999809c288c84d6e9c39383ff76f9ee85a97cae

--- device.mk
+++ device.mk
@@ -60,7 +60,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # GPS
 PRODUCT_PACKAGES += \

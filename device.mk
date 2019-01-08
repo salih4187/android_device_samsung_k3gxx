8ac5f2744dba1326c795653b25145621a1f49a75
Paul Keith <javelinanddart@gmail.com>
2019-01-09 00:47:48 +0100

    k3gxx: Build vendor.lineage.touch HAL from hardware/samsung
    Change-Id: I6eca1e9875cb5793a3a45c6e77bc201946ebd897

--- device.mk
+++ device.mk
@@ -212,6 +212,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libshim_cutils_atomic
 
+# Touch features
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.samsung
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service

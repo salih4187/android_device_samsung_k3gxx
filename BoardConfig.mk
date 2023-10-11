a5c576ff32baf6610162787c050ac1ce9c23eb02
Salih Çağlar <111198755+salih4187@users.noreply.github.com>
2023-10-11 22:41:15 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,12 +17,10 @@
 
 LOCAL_PATH := device/samsung/k3gxx
 
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-
 # Platform
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := exynos5
-TARGET_SLSI_VARIANT := cm
+TARGET_SLSI_VARIANT := bsp
 TARGET_SOC := exynos5422
 
 # Architecture
@@ -35,6 +33,23 @@ TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 ENABLE_CPUSETS := true
 
+# Battery / charging mode
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+BOARD_BATTERY_DEVICE_NAME := battery
+RED_LED_PATH := "/sys/class/leds/sec/led_r/brightness"
+GREEN_LED_PATH := "/sys/class/leds/sec/led_g/brightness"
+BLUE_LED_PATH := "/sys/class/leds/sec/led_b/brightness"
+BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
+CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_HAVE_SAMSUNG_BLUETOOTH := true
+BOARD_CUSTOM_BT_CONFIG := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
 # Bootloader
 TARGET_OTA_ASSERT_DEVICE := k3g,k3gxx
 TARGET_BOOTLOADER_BOARD_NAME := universal5422
@@ -46,21 +61,36 @@ BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21 := true
 BOARD_NEEDS_MEMORYHEAPION := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 
+# ConsumerIR from hardware/samsung
+IR_HAL_SUFFIX := universal5422
+
 # Device Tree
 BOARD_USES_DT := true
 
-# ValidityService
-BOARD_USES_VALIDITY := true
+# Exynos display
+BOARD_USES_VIRTUAL_DISPLAY := true
 
-# Battery / charging mode
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_BATTERY_DEVICE_NAME := battery
-RED_LED_PATH := "/sys/devices/virtual/sec/led/led_r"
-GREEN_LED_PATH := "/sys/devices/virtual/sec/led/led_g"
-BLUE_LED_PATH := "/sys/devices/virtual/sec/led/led_b"
-BACKLIGHT_PATH := "/sys/devices/14400000.fimd_fb/backlight/panel/brightness"
-CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
+# FIMG2D
+BOARD_USES_SKIA_FIMGAPI := true
+
+# Graphics
+USE_OPENGL_RENDERER := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+# Use Exynos BGRA mixer
+BOARD_USE_BGRA_8888 := true
+
+# HDMI
+BOARD_USES_GSC_VIDEO := true
+BOARD_USES_NEW_HDMI := true
+
+# HEVC support in libvideocodec
+BOARD_USE_HEVC_HWIP := true
+
+# HWCServices
+BOARD_USES_HWC_SERVICES := true
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Kernel
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
@@ -71,12 +101,19 @@ TARGET_KERNEL_CONFIG := lineage_k3gxx_defconfig
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+TARGET_KERNEL_CLANG_COMPILE := false
 
-# FIMG2D
-BOARD_USES_SKIA_FIMGAPI := true
+# Lineage hardware
+ifneq ($(findstring lineage, $(TARGET_PRODUCT)),)
+JAVA_SOURCE_OVERLAYS := \
+    org.lineageos.hardware|$(LOCAL_PATH)/lineagehw|**/*.java
+endif
 
-# Include path
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/k3gxx/include
+# NFC
+BOARD_NFC_CHIPSET := pn547
+BOARD_NFC_HAL_SUFFIX := $(TARGET_BOOTLOADER_BOARD_NAME)
+# Legacy BLOB Support
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
 # Partitions
 BOARD_HAS_LARGE_FILESYSTEM := true
@@ -91,60 +128,14 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # Power hal from hardware/samsung
 TARGET_POWERHAL_VARIANT := samsung
 
-# ConsumerIR from hardware/samsung
-IR_HAL_SUFFIX := universal5422
-
 # Radio
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6360
-BOARD_RIL_CLASS := ../../../device/samsung/k3gxx/ril
+BOARD_RIL_CLASS := $(LOCAL_PATH)/ril
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_RECOVERY_FSTAB := device/samsung/k3gxx/ramdisk/fstab.universal5422
-
-# SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/k3gxx/sepolicy
-
-# Graphics
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-# Use Exynos BGRA mixer
-BOARD_USE_BGRA_8888 := true
-
-# Exynos display
-BOARD_USES_VIRTUAL_DISPLAY := true
-
-# HWCServices
-BOARD_USES_HWC_SERVICES := true
-
-# HeartRate
-TARGET_NO_SENSOR_PERMISSION_CHECK := true
-
-# frameworks/native/libs/binder/Parcel.cpp
-TARGET_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
-
-# Samsung OpenMAX Video
-BOARD_USE_STOREMETADATA := true
-BOARD_USE_METADATABUFFERTYPE := true
-BOARD_USE_DMA_BUF := true
-BOARD_USE_ANB_OUTBUF_SHARE := true
-BOARD_USE_IMPROVED_BUFFER := true
-BOARD_USE_GSC_RGB_ENCODER := true
-BOARD_USE_S3D_SUPPORT := true
-BOARD_USE_VP8ENC_SUPPORT := true
-BOARD_USE_HEVCDEC_SUPPORT := true
-BOARD_USE_GSC_RGB_ENCODER := true
-BOARD_USE_ENCODER_RGBINPUT_SUPPORT := true
-TARGET_OMX_LEGACY_RESCALING := true
-
-# HDMI
-BOARD_USES_GSC_VIDEO := true
-BOARD_USES_NEW_HDMI := true
-
-# HEVC support in libvideocodec
-BOARD_USE_HEVC_HWIP := true
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.universal5422
 
 # Samsung OpenMAX Audio
 BOARD_USE_WMA_CODEC := true
@@ -156,6 +147,39 @@ TARGET_SAMSUNG_GRALLOC_EXTERNAL_USECASES := true
 
 # Scaler
 BOARD_USES_SCALER := true
+
+# SECCOMP
+BOARD_SECCOMP_POLICY += $(LOCAL_PATH)/seccomp
+
+# SELinux
+#BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
+
+# Samsung OpenMAX Video
+BOARD_USE_STOREMETADATA := true
+BOARD_USE_METADATABUFFERTYPE := true
+BOARD_USE_DMA_BUF := true
+BOARD_USE_ANB_OUTBUF_SHARE := true
+BOARD_USE_IMPROVED_BUFFER := true
+BOARD_USE_NON_CACHED_GRAPHICBUFFER := true
+BOARD_USE_GSC_RGB_ENCODER := true
+BOARD_USE_CSC_HW := false
+BOARD_USE_QOS_CTRL := false
+BOARD_USE_S3D_SUPPORT := true
+BOARD_USE_TIMESTAMP_REORDER_SUPPORT := false
+BOARD_USE_DEINTERLACING_SUPPORT := false
+BOARD_USE_VP8ENC_SUPPORT := true
+BOARD_USE_HEVCDEC_SUPPORT := true
+BOARD_USE_HEVCENC_SUPPORT := false
+BOARD_USE_HEVC_HWIP := true
+BOARD_USE_VP9DEC_SUPPORT := false
+BOARD_USE_VP9ENC_SUPPORT := false
+BOARD_USE_CUSTOM_COMPONENT_SUPPORT := true
+BOARD_USE_VIDEO_EXT_FOR_WFD_HDCP := false
+BOARD_USE_SINGLE_PLANE_IN_DRM := false
+
+# ValidityService
+BOARD_USES_VALIDITY := true
 
 # WFD
 BOARD_USES_WFD := true
@@ -171,24 +195,3 @@ BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_HAVE_SAMSUNG_BLUETOOTH := true
-BOARD_CUSTOM_BT_CONFIG := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
-
-# NFC
-BOARD_NFC_CHIPSET := pn547
-BOARD_NFC_HAL_SUFFIX := $(TARGET_BOOTLOADER_BOARD_NAME)
-# Legacy BLOB Support
-TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
-
-# CMHW
-BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
-BOARD_HARDWARE_CLASS += device/samsung/k3gxx/cmhw
-
-# SECCOMP
-# frameworks/av/services/{mediacodec,mediaextractor}/minijail
-BOARD_SECCOMP_POLICY += device/samsung/k3gxx/seccomp

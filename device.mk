7be1a82ae733dc57f278ed90dffce1237bd2b4e6
Salih Çağlar <salihcaglar4187@gmail.com>
2025-11-14 12:58:22 +0300

    k3gxx: Build libshim_cutil_atomic

--- device.mk
+++ device.mk
@@ -196,6 +196,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Shims
+PRODUCT_PACKAGES += \
+    libshim_cutils_atomic
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service

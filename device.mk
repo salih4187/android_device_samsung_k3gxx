62ae706458c2b0e7996ddb5f09142fce79fd943b
Salih Çağlar <salihcaglar4187@gmail.com>
2026-06-12 15:49:10 +0300

    k3gxx: Switch to HIDL Light HAL

--- device.mk
+++ device.mk
@@ -106,8 +106,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.universal5422
+    android.hardware.light@2.0-service.samsung
 
 # LiveDisplay
 PRODUCT_PACKAGES += \

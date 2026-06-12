2a3dfb49ba60749eb884a9e4bb6091b131b06421
arco <arco68@gmail.com>
2026-06-12 15:31:12 +0300

    k3gxx: Build common legacy gnss hal Change-Id: I56095b9c3bba065779c90b6aeb61ebfed6af7e77

--- device.mk
+++ device.mk
@@ -69,8 +69,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    android.hardware.gnss@1.0-impl.legacy \
+    android.hardware.gnss@1.0-service.legacy
 
 # SHIM LIBS
 PRODUCT_PACKAGES += \
